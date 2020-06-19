a531c1b8eb19dba7641f4f23feac75bf7aad90aa
UM4RFAH <67075032+UM4RFAH@users.noreply.github.com>
2020-06-20 01:36:14 +0800

    Add files via upload

---
+++ umar.sh
@@ -0,0 +1,261 @@
+#!/bin/sh
+pkg update -y
+pkg upgrade -y
+pkg install ruby -y
+pkg install git -y
+pkg install figlet -y
+pkg install python python2 -y
+pkg install toilet
+pkg install gem -y
+gem install lolcat
+clear
+toilet -f big -F gay "welcome"
+sleep 3
+figlet "LOADING"|lolcat
+sleep 1
+echo "Please WAIT"|lolcat
+sleep 3
+echo "starting tools    ";
+sleep 0.1;
+clear
+echo "Starting tools.   ";
+sleep 0.1;
+clear
+echo "sTarting tools..  ";
+sleep 0.1;
+clear
+echo "stArting tools... ";
+sleep 0.1;
+clear
+echo "staRting tools    ";
+sleep 0.1;
+clear
+echo "starTing tools.   ";
+sleep 0.1;
+clear
+echo "startIng tools..  ";
+sleep 0.1;
+clear
+echo "startiNg tools... ";
+sleep 0.1;
+clear
+echo "startinG tools    ";
+sleep 0.1;
+clear
+echo "starting Tools.   ";
+sleep 0.1;
+clear
+echo "starting tOols..  ";
+sleep 0.1;
+clear
+echo "starting toOls... ";
+sleep 0.1;
+clear
+echo "starting tooLs    ";
+sleep 0.1;
+clear
+echo "starting toolS.   ";
+sleep 0.1;
+clear
+echo "starting tools..  ";
+sleep 0.1;
+clear
+echo "Starting tools... ";
+sleep 0.1;
+clear
+echo "sTarting tools    ";
+sleep 0.1;
+clear
+echo "stArting tools.   ";
+sleep 0.1;
+clear
+echo "staRting tools..  ";
+sleep 0.1;
+clear
+echo "starTing tools... ";
+sleep 0.1;
+clear
+echo "startIng tools    ";
+sleep 0.1;
+clear
+echo "startiNg tools.   ";
+sleep 0.1;
+clear
+echo "startinG tools..  ";
+sleep 0.1;
+clear
+echo "starting Tools... ";
+sleep 0.1;
+clear
+echo "starting tOols    ";
+sleep 0.1;
+clear
+echo "starting toOls.   ";
+sleep 0.1;
+clear
+echo "starting tooLs..  ";
+sleep 0.1;
+clear
+echo "starting toolS... ";
+sleep 0.1;
+clear
+echo "starting tools    ";
+sleep 0.1;
+clear
+echo "Starting tools.   ";
+sleep 0.1;
+clear
+echo "sTarting tools... ";
+sleep 0.1;
+clear
+echo "stArting tools    ";
+sleep 0.1;
+clear
+echo "staRting tools.   ";
+sleep 0.1;
+clear
+echo "starTing tools..  ";
+sleep 0.1;
+clear
+echo "startIng tools... ";
+sleep 0.1;
+clear
+echo "startiNg tools    ";
+sleep 0.1;
+clear
+echo "startinG tools.   ";
+sleep 0.1;
+clear
+echo "starting Tools..  ";
+sleep 0.1;
+clear
+echo "starting tOols... ";
+sleep 0.1;
+clear
+echo "starting toOls    ";
+sleep 0.1;
+clear
+echo "starting tooLs.   ";
+sleep 0.1;
+clear
+echo "starting toolS..  ";
+sleep 0.1;
+clear
+echo "     #########################################"|lolcat
+echo "                       _ _ "|lolcat
+echo "                   o/ \ili/ \o "|lolcat
+echo "                      (o o) "|lolcat
+echo "                ooO--(/___\)--Ooo"|lolcat
+echo "               (===UMAR GANSS===)"|lolcat
+echo "Author    : UMAR GANSS"|lolcat
+echo "My Team   : F0URSD34TH T34M"|lolcat
+echo "My Team   : ANONYMOUS CYBER MUSLIM INDONESIA/Xiuz code"|lolcat
+echo "Thanks To : Mr.13DL,Mr.S3P0NG#013"|lolcat
+echo "            Umar Ganss,Abhay Zahama"|lolcat
+echo "            Mr.OxyGeini7,Mr.Z4MU3L,Raka Rohman"|lolcat
+echo "            Mr.R1Z4L,1NS4Y N3T,Mr.ProjEcT Er0R"|lolcat
+echo
+echo "     #########################################"|lolcat
+echo
+echo "       <<<<<<<<<<<<<<<<<=>>>>>>>>>>>>>>>>>"|lolcat
+echo "      [-] WELCOME TO MY TOOLS INSTALLER [-]"|lolcat
+echo "       <<<<<<<<<<<<<<<<<=>>>>>>>>>>>>>>>>>"|lolcat
+echo
+echo "================================"|lolcat
+echo "     -=Hack Facebook =-      "|lolcat
+echo "================================"|lolcat
+echo "1.) OSIF"|lolcat
+echo "2.) fbbrute"|lolcat
+echo "================================"|lolcat
+echo "    -=Kumpulan--Tools =-     "|lolcat
+echo "================================"|lolcat
+echo "3.) cybertools"|lolcat
+echo "4.) ZTOOLS"|lolcat
+echo "================================"|lolcat
+echo "     -= DDOS ATTACK =-      "|lolcat
+echo "================================"|lolcat
+echo "5.) Hammer"|lolcat
+echo "6.) Lite DDOS"|lolcat
+echo "================================"|lolcat
+echo $green
+read -p "ᏬᎷᏗᏒ_ᎶᏗᏁᏕᏕ #Pilih Angka = " BDL;
+
+if [ $BDL = 1 ] || [ $BDL = 1 ]
+then
+clear
+toilet -f big -F gay "osif"
+sleep 3
+pkg install python2
+git clone https://github.com/ciku370/OSIF
+cd OSIF
+pip2 install -r requirements.txt
+python2 osif.py
+fi
+
+if [ $BDL = 2 ] || [ $BDL = 2 ]
+then
+clear
+toilet -f big -F gay "fbbrute"
+sleep 3
+apt update && apt upgrade
+Pkg install python2
+Pkg install python2-dev
+Pkg install wget
+git clone https://github.com/Senitopeng/fbbrute
+cd fbbrute
+python2 fbbrute.py
+fi
+
+if [ $BDL = 3 ] || [ $BDL = 3 ]
+then
+clear
+toilet -f big -F gay "cybertools"
+sleep 3
+pkg install python2 -y
+pkg install git -y
+git clone https://github.com/BadruArmy/cybertools
+cd cybertools
+sh new.sh
+fi
+
+if [ $BDL = 4 ] || [ $BDL = 4 ]
+then
+clear
+toilet -f big -F gay "ZTOOLS"
+sleep 3
+apt update -y
+apt upgrade -y
+pkg install git -y
+pkg install python2 -y
+git clone https://github.com/rayyanaa/ZTOOLS
+cd ZTOOLS
+sh K4MBINGHITAM.sh
+fi
+
+if [ $BDL = 5 ] || [ $BDL = 5 ]
+then
+clear
+toilet -f big -F gay "Hammer"
+sleep 3
+pkg update -y
+pkg upgrade -y
+pkg install python -y
+pkg install git -y
+git clone https://github.com/cyweb/hammer
+cd hammer
+python hammer.py
+fi
+
+if [ $BDL = 6 ] || [ $BDL = 6 ]
+then
+clear
+toilet -f big -F gay "LITEDDOS"
+sleep 3
+apt update -y
+apt upgrade -y
+pkg install git -y
+pkg install python2 -y
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+python2 liteDDOS.py
+fi
